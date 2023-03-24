ec07db1c3886b80a96abcbf8279b6dff6c11db12
MuteHommie <muthomi4oscar@gmail.com>
2023-03-25 00:52:32 +0300

    Initial commit
    This are the files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame implements ActionListener {
+
+    public  Main(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
